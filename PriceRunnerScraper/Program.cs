5e75bcbc7479b23dc8fcfb18b55ff63437665c3f
Michael Lauritsen <i30288741@live.dk>
2018-11-15 09:38:59 +0100

    Insert search request for PriceRunner.dk very basic

--- PriceRunnerScraper/Program.cs
+++ PriceRunnerScraper/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
+using HtmlAgilityPack;
 using System.Threading.Tasks;
 
 namespace PriceRunnerScraper
@@ -10,6 +12,58 @@ namespace PriceRunnerScraper
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Hvad vil du søge efter på PriceRunner?");
+                GetHTMLAsync();
+                
+            }
+            
+        }
+
+        private static async void GetHTMLAsync()
+        {
+            // Search Input Using Pricerunner.dk
+            string q = Console.ReadLine();            
+            var url = "https://www.pricerunner.dk/results?q= "+ q;
+            Console.Clear();
+
+            // Using the http client with Async
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            // Load the html document
+            var HtmlDocument = new HtmlDocument();
+            HtmlDocument.LoadHtml(html);
+
+
+            var ProductList = HtmlDocument.DocumentNode.Descendants("ol")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("_3s8cb94L7l")).ToList();
+
+            var ProductListItems = ProductList[0].Descendants("li")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Contains("_25C076-hun")).ToList();
+
+            foreach (var product in ProductListItems)
+            {
+                // the id class (dont wonna show it)
+                //Console.WriteLine(product.GetAttributeValue("class", ""));
+
+                // the product name
+                Console.WriteLine(product.Descendants("h3")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("_2toT0OEaJp _4lEE4_tUxu _3D5OyasTJT _1wgy7yB_H3")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                // The Price
+                Console.WriteLine(product.Descendants("span")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("_1ELjwOz8XJ _2-X5EtVv_D")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
