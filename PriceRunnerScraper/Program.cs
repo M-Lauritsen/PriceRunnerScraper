22773cfaf4eb7873d3d96fe57fe6d6965f2ffad3
Michael Lauritsen <i30288741@live.dk>
2018-11-21 08:38:00 +0100

    små forbedringer

--- PriceRunnerScraper/Program.cs
+++ PriceRunnerScraper/Program.cs
@@ -11,61 +11,78 @@ namespace PriceRunnerScraper
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            start:
+            Console.Clear();
             Console.WriteLine("Hvad vil du søge efter på PriceRunner?");
             GetHTMLAsync();
             Console.ReadKey();
-
+            goto start;
         }
 
         private static async void GetHTMLAsync()
         {
-            // Search Input Using Pricerunner.dk
-            string q = Console.ReadLine();            
-            var url = "https://www.pricerunner.dk/results?q= "+ q;
-            Console.Clear();
-
-            // Using the http client with Async
-            var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(url);
-
-            // Load the html document
-            var HtmlDocument = new HtmlDocument();
-            HtmlDocument.LoadHtml(html);
-
-
-            var ProductList = HtmlDocument.DocumentNode.Descendants("ol")
-                .Where(node => node.GetAttributeValue("class", "")
-                .Equals("_3s8cb94L7l")).ToList();
-
-            var ProductListItems = ProductList[0].Descendants("li")
-                .Where(node => node.GetAttributeValue("class", "")
-                .Contains("_25C076-hun")).ToList();
-
-            foreach (var product in ProductListItems)
+            try
             {
-                // the id class (dont wonna show it)
-                //Console.WriteLine(product.GetAttributeValue("class", ""));
+                // Search Input Using Pricerunner.dk
+                string q = Console.ReadLine();
+                var url = "https://www.pricerunner.dk/results?q= " + q;
+                Console.Clear();
+                Console.WriteLine($"Søger efter {q}...\n");
+                // Using the http client with Async
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
 
-                // the product name
-                Console.WriteLine(product.Descendants("h3")
+                // Load the html document
+                var HtmlDocument = new HtmlDocument();
+                HtmlDocument.LoadHtml(html);
+
+
+                var ProductList = HtmlDocument.DocumentNode.Descendants("ol")
                     .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("_2toT0OEaJp _4lEE4_tUxu _3D5OyasTJT _1wgy7yB_H3")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+                    .Equals("_3s8cb94L7l")).ToList();
 
-                // Product info
-                Console.WriteLine(product.Descendants("p")
+                var ProductListItems = ProductList[0].Descendants("li")
                     .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("_2k9_q2p_9L _1-53aS_lww tBxFeb0uqB")).FirstOrDefault().InnerText.Trim('\r','\n','\t'));
+                    .Contains("_25C076-hun")).ToList();
 
-                // The Price
-                Console.WriteLine(product.Descendants("span")
-                    .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("_1ELjwOz8XJ _2-X5EtVv_D")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
 
-                Console.WriteLine();
+                foreach (var product in ProductListItems)
+                {
+                    try
+                    {
+                        // the product name
+                        Console.WriteLine(product.Descendants("h3")
+                            .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("_2toT0OEaJp _4lEE4_tUxu _3D5OyasTJT _1wgy7yB_H3")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                        // Product info
+                        Console.WriteLine(product.Descendants("p")
+                            .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("_2k9_q2p_9L _1-53aS_lww tBxFeb0uqB")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                        // The Price
+                        Console.WriteLine(product.Descendants("span")
+                            .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("_1ELjwOz8XJ _2-X5EtVv_D")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                        
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Found nothing");
+                    }
+                    Console.WriteLine();
+                }
+            
             }
+            catch (Exception)
+            {
 
-            Console.ReadKey();
+                Console.WriteLine("Found nothing");
+            }
         }
+
     }
 }
